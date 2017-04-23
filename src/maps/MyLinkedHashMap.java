a8477e1a3c7c11acd69e5c1717e5d68bd45cdcc8
MustafaGungor <mustafagungormg@outlook.com>
2017-04-23 22:41:24 +0300

    LinkedHasMap and TreeMap changes

--- src/maps/MyLinkedHashMap.java
+++ src/maps/MyLinkedHashMap.java
@@ -1,8 +1,24 @@
 package maps;
 
+import java.util.LinkedHashMap;
+import java.util.Set;
+
 /**
  * Created by Diagram on 23/04/2017.
  */
 public class MyLinkedHashMap {
+    public MyLinkedHashMap(){
+        LinkedHashMap linkedHashMap = new LinkedHashMap();
 
+        //ASCII tablosundaki 65-90 arasındaki değerleri ve anahtarları mapimize koyuyoruz
+        for (int i=65;i<=90;i++)
+            linkedHashMap.put((char)i,i);
+
+        Set anahtarlar =linkedHashMap.keySet();
+        System.out.println("LinkedHashMap Öğeleri");
+        System.out.println(linkedHashMap);
+        System.out.println("LinkedHashMap öğesinin anahtarları");
+        System.out.println(anahtarlar);
+
+    }
 }
